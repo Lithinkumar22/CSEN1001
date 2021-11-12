678ec4d63b2f45dc64caca1df9d88ac3a044d4a7
k.Lithinkumar <93367914+Punugulutindam@users.noreply.github.com>
2021-11-12 10:21:49 +0000

    my details

---
+++ Takingdetails.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+printf("name:k.lithin\n");
+printf("DOB:22/07/2002\n");
+printf("mother:bhargavi\n");
+printf("father:venugopalrao\n");
+printf("brother:jagath\n");
+}
