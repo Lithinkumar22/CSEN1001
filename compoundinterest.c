34897c8113d7279088080248b86dddf54fb20c83
k.Lithinkumar <93367914+Punugulutindam@users.noreply.github.com>
2021-11-25 14:33:37 +0530

    Add files via upload

--- compoundinterest.c
+++ compoundinterest.c
@@ -1,15 +1,8 @@
-#include <stdio.h>
+#include<stdio.h>
 int main(){
 	int p,t,r;
 	printf("principle=");scanf("%d",&p);
 	printf("rate of interest=");scanf("%d",&r);
 	printf("time=");scanf("%d",&t);
-	printf("simple interest=%d\n",(p*t*r/100));	
-	
-	
-	
-	
-	
-	
+	printf("compound interest=%d\n",(p*(1+r/100)*t));
 }
-
