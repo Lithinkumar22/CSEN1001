d6644a7c6584fbe09bcb8c8b3a9c5beace6dee0e
k.Lithinkumar <93367914+Punugulutindam@users.noreply.github.com>
2021-12-16 09:14:21 +0000

    pascal program

--- remainder.c
+++ remainder.c
@@ -1,14 +1,13 @@
-# include <stdio.h>
-int main() {
-    int number, divisor;
-    
-    printf("Enter an integer : ");
-    scanf("%d", &number);
+#include<stdio.h>
+int main(){
+  int a,b;
+  printf("enter a=");scanf("%d",&a);
+  printf("enter b=");scanf("%d",&b);
+  printf("remainder=%d\n",a-b*(a/b));
 
-    printf("Enter divisor : ");
-    scanf("%d", &divisor);
 
-    printf("Reminder = %d\n", number - divisor * (number/divisor));
-    
-    return 0;
+
+
+
+
 }
