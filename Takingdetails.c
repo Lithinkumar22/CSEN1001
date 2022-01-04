236542782dfe3a1d261104c4d22f6d1b125c9768
k.Lithinkumar <93367914+Punugulutindam@users.noreply.github.com>
2022-01-04 13:14:21 +0530

    Delete Takingdetails.c

--- Takingdetails.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-printf("name:k.lithin\n");
-printf("DOB:22/07/2002\n");
-printf("mother:bhargavi\n");
-printf("father:venugopalrao\n");
-printf("brother:jagath\n");
-}
